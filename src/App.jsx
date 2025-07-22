ad71854b923d2b89cdc61e3315dd9ed73b155980
sravya-1605 <asravya384@gmail.com>
2025-07-22 12:51:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/s207">
+    <Router basename="/skill-3">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
